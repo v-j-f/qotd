4281bafdf9a966285a02ba379d148a629f3d4c4b
vjfernan <vjfernan@gmail.com>
2020-07-30 23:25:35 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+qotd: Quote of the day
+-----------------------------
+
+qotd is a program in Python that it show a quote of the day in the terminal.
+
+To install run:
+
+$ sudo bash -c "$(curl -sL https://github.com/vjfernan/qotd/raw/master/install.sh)" >&2
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+readonly QOTD_DOWNLOAD_URL="https://bitbucket.org/vjavierf/qotd/raw/master/qotd.py"
+readonly QOTD_DIRECTORY="/usr/local/bin"
+
+if [ ${EUID} -ne 0 ]; then
+    echo "This script must be run as root." >&2
+    exit 1
+fi
+if ! [[ -t 0 ]]; then
+    echo "This script is interactive, please run: bash -c \"\$(curl https://bitbucket.org/vjavierf/qotd/raw/master/install.sh)\"" >&2
+    exit 1
+fi
+if ! [[ -f "$QOTD_DIRECTORY/qotd" ]]; then
+    curl -L $QOTD_DOWNLOAD_URL -o /tmp/qotd
+    if  [ $? != 0 ]; then
+        echo "Error downloading qotd."
+        exit 1
+    else
+        mv /tmp/qotd $QOTD_DIRECTORY/
+        chmod 755 $QOTD_DIRECTORY/qotd
+    	echo "qotd installed."
+    	echo "Run qotd to show a quote of the day."
+    fi
+else
+    echo "You already have qotd installed."
+    exit 1
+fi
